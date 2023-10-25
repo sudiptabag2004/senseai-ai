ff63cb9d5de8f314677f6a0d82651d7406b2fae6
Aman Dalmia <aman.d@hyperverge.co>
2023-10-25 12:39:35 +0530

    FIX: site packages path for prod

--- update_langchain.sh
+++ update_langchain.sh
@@ -4,6 +4,7 @@ else
     SITE_PACKAGES_DIR=./venv/lib/python3.10
 fi
 
+SITE_PACKAGES_DIR=/usr/local/lib/python3.8/
 # echo "$SITE_PACKAGES_DIR/site-packages/langchain/chat_models/base.py"
 
 cp langchain/base.py "$SITE_PACKAGES_DIR/site-packages/langchain/chat_models/base.py"
