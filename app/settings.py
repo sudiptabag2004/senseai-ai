3d862b2781561950ed87fd56bde35c03e8a35135
Aman Dalmia <aman.d@hyperverge.co>
2023-12-19 19:09:23 +0530

    FIX: add back env file function

--- app/settings.py
+++ app/settings.py
@@ -2,13 +2,17 @@ import os
 from pydantic import BaseSettings
 
 
+def get_env_file_path():
+    return ".env"
+
+
 class Settings(BaseSettings):
     openai_api_key: str
     openai_org_id: str
-    wandb_api_key: str
+    # wandb_api_key: Optional[str]
 
     class Config:
-        env_file = ".env"
+        env_file = get_env_file_path()
 
 
 settings = Settings()
