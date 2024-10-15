44faefd833b5f729f94b8f376ac0eab6c16a1572
Aman Dalmia <aman.d@hyperverge.co>
2024-10-15 14:11:28 +0000

    Add Sidebar links

--- src/home.py
+++ src/home.py
@@ -14,10 +14,16 @@ init_db()
 if "email" in st.query_params:
     st.session_state.email = st.query_params["email"]
 
+if "is_hv_learner" in st.query_params:
+    st.session_state.is_hv_learner = int(st.query_params["is_hv_learner"])
+
 st.title("SensAI - your personal AI tutor")
 
 if "email" not in st.session_state:
     st.session_state.email = None
+
+if "is_hv_learner" not in st.session_state:
+    st.session_state.is_hv_learner = False
 
 
 def clear_auth():
@@ -82,6 +88,44 @@ if st.session_state.email:
         clear_auth()
 
 menu()
+
+
+def show_links():
+    st.sidebar.link_button(
+        "🏖️ Apply for a leave",
+        "https://script.google.com/macros/s/AKfycbzfH42BZ5t8Yo9O1B23ELpM920EDwQ2_1ecOpk4OGqN3bMZ_FURie8uzHLtlKp-CooC/exec",
+    )
+    st.sidebar.link_button(
+        "🎙️ Apply for a mock interview",
+        "https://script.google.com/a/macros/hyperverge.co/s/AKfycbz14hcx16xpn5DklkSa_n28tfIVi8v9oXzDHYJVBBcTwZIDZZ3s6QpBD7WLEyPhKrxc/exec?page=interview_scheduling",
+    )
+    st.sidebar.link_button(
+        "😎 Sentence Enhancer",
+        "https://chromewebstore.google.com/detail/sentence-enhancer/gomkfbbonafokpdagofhfkhoaeamcjmf?authuser=0&hl=en",
+    )
+    st.sidebar.link_button(
+        "🗣️ Power Up (your English skills)",
+        "https://wa.me/916366309432",
+        help="Text `English` to get started",
+    )
+
+
+st.sidebar.divider()
+
+
+def update_query_params(
+    key,
+):
+    st.query_params[key] = int(st.session_state[key])
+
+
+if st.sidebar.checkbox(
+    "I am a HyperVerge Academy learner",
+    key="is_hv_learner",
+    on_change=update_query_params,
+    args=("is_hv_learner",),
+):
+    show_links()
 
 if not st.query_params:
     st.query_params.clear()
