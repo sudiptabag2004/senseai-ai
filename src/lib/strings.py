5bd687d85e29f9b2c4449926ad700aa561889b53
Aman Dalmia <aman.d@hyperverge.co>
2024-11-29 13:21:49 +0000

    Support for Tasks with custom criteria and report format

--- src/lib/strings.py
+++
@@ -1,8 +0,0 @@
-admin_show_code_editor_label = "This is a coding task"
-admin_show_code_editor_help = (
-    "Whether the task is a programming task requiring the learner to write code"
-)
-admin_code_editor_language_label = "Code editor language (s)"
-admin_code_editor_language_help = "Choose or more languages to show in the code editor"
-
-admin_code_test_cases_label = "Add tests"
