fd67feb500815bbd78e607ba238c10a919eeed8a
Aman Dalmia <amandalmia18@gmail.com>
2025-01-25 18:18:35 +0530

    FIX: cleanup db file

--- src/lib/utils/db.py
+++ src/lib/utils/db.py
@@ -8,76 +8,6 @@ from lib.config import sqlite_db_path
 from lib.utils.logging import logger
 
 
-class DatabaseConnectionPool:
-    def __init__(self, sqlite_db_path, max_connections=5):
-        self.sqlite_db_path = sqlite_db_path
-        self.max_connections = max_connections
-        self.connections = queue.Queue(maxsize=max_connections)
-        self.active_connections = 0
-        self.lock = threading.Lock()
-
-    def get_connection(self):
-        try:
-            conn = self.connections.get_nowait()
-            logger.info("Connection retrieved from pool.")
-            return conn
-        except queue.Empty:
-            with self.lock:
-                if self.active_connections < self.max_connections:
-                    self.active_connections += 1
-
-                    conn = sqlite3.connect(self.sqlite_db_path, check_same_thread=False)
-
-                    conn.set_trace_callback(self.trace_callback)
-
-                    current_busy_timeout = conn.execute(
-                        "PRAGMA busy_timeout;"
-                    ).fetchone()[0]
-
-                    conn.execute("PRAGMA synchronous=NORMAL;")
-
-                    logger.info(
-                        f"New connection created with busy timeout: {current_busy_timeout}"
-                    )
-
-                    return conn
-
-            conn = self.connections.get()
-            logger.info("Connection retrieved from pool after waiting.")
-            return conn
-
-    def return_connection(self, connection):
-        if connection:
-            try:
-                self.connections.put_nowait(connection)
-                logger.info("Connection returned to pool.")
-            except queue.Full:
-                connection.close()
-                with self.lock:
-                    self.active_connections -= 1
-                logger.info("Connection closed due to full pool.")
-
-    def trace_callback(self, sql):
-        # Record the start time and SQL
-        logger.info(f"Executing operation: {sql}")
-        DatabaseConnectionPool.current_sql = sql
-        DatabaseConnectionPool.start_time = time.time()
-
-    def profile_callback(self):
-        print("here")
-        # Calculate duration and log if exceeds threshold
-        end_time = time.time()
-        duration = end_time - DatabaseConnectionPool.start_time
-        # if duration > 0.005:  # 5ms threshold
-        logger.error(
-            f"Long-running query ({duration:.3f}s): {DatabaseConnectionPool.current_sql}"
-        )
-
-    # Initialize class variables
-    current_sql = ""
-    start_time = 0
-
-
 def trace_callback(sql):
     # Record the start time and SQL
     logger.info(f"Executing operation: {sql}")
@@ -90,36 +20,6 @@ def get_new_db_connection():
     conn.set_trace_callback(trace_callback)
 
     return conn
-
-
-global_db_pool = DatabaseConnectionPool(
-    sqlite_db_path=sqlite_db_path, max_connections=5
-)
-
-
-@contextmanager
-# def get_shared_db_connection():
-#     # TODO: reverting the db_pool implementation here is not correct. a new pool is being used on every call instead of using one shared pool
-#     # if we create one shared pool, we get an SQLite programming error: SQLite objects created in a thread can only be used in that same thread.
-#     # We can disable this check but then, we will need to handle race conditions ourselves. Reading should be fine but insertion, update, delete # needs to happen with locks in place, a single writer queue for SQLite.
-#     # For our use case, since we have a lot of writes that are going to happen, a single write queue might be a major block. Might be better
-#     # to shift to postgres DB
-
-#     # connection = None
-#     # try:
-#     #     connection = get_new_db_connection()
-#     #     yield connection
-#     # finally:
-#     #     if connection:
-#     #         connection.close()
-
-#     connection = None
-#     try:
-#         connection = global_db_pool.get_connection()
-#         yield connection
-#     finally:
-#         if connection:
-#             global_db_pool.return_connection(connection)
 
 
 def set_db_defaults():
