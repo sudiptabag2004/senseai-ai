06cb1d4ab87019a19909500e1442db41bf0307b3
Aman Dalmia <aman.d@hyperverge.co>
2025-02-02 16:29:37 +0000

    Show full leaderboard with exact ranks

--- src/components/leaderboard.py
+++ src/components/leaderboard.py
@@ -7,23 +7,64 @@ from models import LeaderboardViewType
 from .base import set_box_style, show_box_header
 
 
-def show_user_info(name, email, streak, tasks, rank, show_separator=False):
+def show_user_info(
+    name,
+    email,
+    streak,
+    tasks,
+    rank,
+    show_separator=False,
+    highlight_user_row: bool = False,
+):
     if show_separator:
         st.markdown('<div class="separator"></div>', unsafe_allow_html=True)
+
+    is_logged_in_user = email == st.session_state.email
 
     col1, col2 = st.columns([1, 5])
 
     with col1:
-        medal_image = open(f"lib/assets/leaderboard_{rank}.svg").read()
-        st.markdown(
-            f'<div style="display: flex; justify-content: center; align-items: center; height: 100%;">'
-            f"{medal_image}"
-            f"</div>",
-            unsafe_allow_html=True,
-        )
+        if rank is not None and rank <= 3:
+            medal_image = open(f"lib/assets/leaderboard_{rank}.svg").read()
+            st.markdown(
+                f'<div style="display: flex; justify-content: center; align-items: center; height: 100%;">'
+                f"{medal_image}"
+                f"</div>",
+                unsafe_allow_html=True,
+            )
+        else:
+            if st.session_state.theme["base"] == "dark":
+                text_color = "white"
+                border_style = "border: 1px solid white;"
+                highlight_background_color = "#424bf5"
+            else:
+                text_color = "black"
+                border_style = "border: 1px solid black;"
+                highlight_background_color = "#b6d9e0"
+
+            if rank is None:
+                rank = "-"
+                border_style = ""
+
+            if highlight_user_row and is_logged_in_user:
+                background_color_style = (
+                    f"background-color: {highlight_background_color};"
+                )
+            else:
+                background_color_style = ""
+
+            st.markdown(
+                f'<div style="display: flex; justify-content: center; align-items: center; height: 100%;">'
+                f'<div style="width: 30px; height: 30px; border-radius: 50%; {border_style} {background_color_style} display: flex; justify-content: center; align-items: center;">'
+                f'<span style="color: {text_color};">{rank}</span>'
+                f"</div>"
+                f"</div>",
+                unsafe_allow_html=True,
+            )
 
     with col2:
-        user_text = " (You)" if email == st.session_state.email else ""
+        user_text = " (You)" if is_logged_in_user else ""
+
         st.markdown(
             f'<p style="text-decoration: none; margin-bottom: 0;">{name}{user_text}</p>'
             f'<p style="margin-top: 0;">Streak: {streak} | Tasks Completed: {tasks}</p>',
@@ -31,7 +72,7 @@ def show_user_info(name, email, streak, tasks, rank, show_separator=False):
         )
 
 
-def get_top_performers(view_type: LeaderboardViewType, cohort_id: int):
+def get_leaderboard(view_type: LeaderboardViewType, cohort_id: int):
     if view_type not in leaderboard_view_types:
         raise ValueError(f"Invalid view type: {view_type}")
 
@@ -58,6 +99,7 @@ def get_top_performers(view_type: LeaderboardViewType, cohort_id: int):
     sorted_users = sorted(users_data, key=lambda x: (x[-2], x[-1]), reverse=True)
 
     top_performers = []
+    other_performers = []
     previous_streak = None
     previous_tasks_completed = None
     rank = 0
@@ -74,27 +116,39 @@ def get_top_performers(view_type: LeaderboardViewType, cohort_id: int):
             tasks_completed = tasks_completed
 
         if rank > 3:
-            break
+            other_performers.append((name, email, streak_count, tasks_completed, rank))
+            continue
 
         top_performers.append((name, email, streak_count, tasks_completed, rank))
 
-    return top_performers
+    return top_performers, other_performers
 
 
-def show_leaderboard(cohort_id: int):
-    set_box_style()
+def _show_leaderboard(cohort_id: int, view_type: Literal["top", "full"] = "top"):
 
-    with st.container(border=True):
-        show_box_header("Top Performers")
+    tabs = st.tabs(leaderboard_view_types)
 
-        tabs = st.tabs(leaderboard_view_types)
+    container_kwargs = {}
 
-        for tab_index, tab in enumerate(tabs):
-            with tab:
-                top_performers = get_top_performers(
+    if view_type == "full":
+        container_kwargs = {
+            "height": 500,
+            "border": False,
+        }
+
+    for tab_index, tab in enumerate(tabs):
+        with tab:
+            with st.container(**container_kwargs):
+                top_performers, other_performers = get_leaderboard(
                     leaderboard_view_types[tab_index], cohort_id
                 )
-                is_logged_in_user_top_performer = False
+                is_logged_in_user_performer = False
+
+                performers_to_show = (
+                    top_performers
+                    if view_type == "top"
+                    else top_performers + other_performers
+                )
 
                 for index, (
                     name,
@@ -102,7 +156,7 @@ def show_leaderboard(cohort_id: int):
                     streak_count,
                     tasks_completed,
                     rank,
-                ) in enumerate(top_performers):
+                ) in enumerate(performers_to_show):
                     show_user_info(
                         name,
                         email,
@@ -110,32 +164,64 @@ def show_leaderboard(cohort_id: int):
                         tasks_completed,
                         rank,
                         show_separator=index != 0,
+                        highlight_user_row=view_type == "full",
                     )
                     if st.session_state.email == email:
-                        is_logged_in_user_top_performer = True
+                        is_logged_in_user_performer = True
 
-                if not is_logged_in_user_top_performer:
-                    streak_count = (
-                        len(get_user_streak(st.session_state.user["id"], cohort_id))
-                        or 0
-                    )
-                    tasks_completed = (
-                        len(
-                            get_solved_tasks_for_user(
-                                st.session_state.user["id"],
-                                cohort_id,
-                                leaderboard_view_types[tab_index],
+                if not is_logged_in_user_performer:
+                    logged_in_user_info = [
+                        info
+                        for info in other_performers
+                        if info[1] == st.session_state.email
+                    ]
+
+                    if not logged_in_user_info:
+                        logged_in_user_info = (
+                            get_display_name_for_user(st.session_state.user),
+                            st.session_state.user["email"],
+                            0,
+                            len(
+                                get_solved_tasks_for_user(
+                                    st.session_state.user["id"],
+                                    cohort_id,
+                                    leaderboard_view_types[tab_index],
+                                )
                             ),
+                            None,
                         )
-                        or 0
-                    )
+                    else:
+                        logged_in_user_info = logged_in_user_info[0]
+
                     show_separator = len(top_performers) > 0
 
                     show_user_info(
-                        get_display_name_for_user(st.session_state.user),
-                        st.session_state.user["email"],
-                        streak_count,
-                        tasks_completed,
-                        rank=4,
+                        logged_in_user_info[0],
+                        logged_in_user_info[1],
+                        logged_in_user_info[2],
+                        logged_in_user_info[3],
+                        logged_in_user_info[4],
                         show_separator=show_separator,
+                        highlight_user_row=view_type == "full",
                     )
+
+
+@st.dialog("Leaderboard")
+def show_full_leaderboard_dialog(cohort_id: int):
+    _show_leaderboard(cohort_id=cohort_id, view_type="full")
+
+
+def show_leaderboard(cohort_id: int):
+    set_box_style()
+
+    with st.container(border=True):
+        show_box_header("Top Performers")
+        _show_leaderboard(cohort_id=cohort_id, view_type="top")
+
+    st.button(
+        "Show Full Leaderboard",
+        type="primary",
+        use_container_width=True,
+        on_click=show_full_leaderboard_dialog,
+        args=(cohort_id,),
+    )
