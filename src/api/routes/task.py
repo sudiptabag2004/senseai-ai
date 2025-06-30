4cdf519892e81ab54b1b9b30958e6a633d62e0e0
Aman Dalmia <amandalmia18@gmail.com>
2025-06-30 10:52:03 +0530

    fix missing imports

--- src/api/routes/task.py
+++ src/api/routes/task.py
@@ -3,16 +3,9 @@ from typing import List, Dict
 from api.db import (
     get_solved_tasks_for_user as get_solved_tasks_for_user_from_db,
     get_task as get_task_from_db,
-    get_scoring_criteria_for_task as get_scoring_criteria_for_task_from_db,
     delete_task as delete_task_in_db,
     delete_tasks as delete_tasks_in_db,
-    get_scoring_criteria_for_tasks as get_scoring_criteria_for_tasks_from_db,
-    add_tags_to_task as add_tags_to_task_in_db,
-    remove_tags_from_task as remove_tags_from_task_in_db,
-    add_scoring_criteria_to_tasks as add_scoring_criteria_to_tasks_in_db,
-    remove_scoring_criteria_from_task as remove_scoring_criteria_from_task_in_db,
     get_courses_for_tasks as get_courses_for_tasks_from_db,
-    update_tests_for_task as update_tests_for_task_in_db,
     create_draft_task_for_course as create_draft_task_for_course_in_db,
     update_learning_material_task as update_learning_material_task_in_db,
     update_draft_quiz as update_draft_quiz_in_db,
@@ -140,25 +133,6 @@ async def get_courses_for_tasks(
     return await get_courses_for_tasks_from_db(task_ids)
 
 
-@router.get("/scoring_criteria")
-async def get_scoring_criteria_for_tasks(task_ids: List[int] = Query(...)):
-    return await get_scoring_criteria_for_tasks_from_db(task_ids)
-
-
-@router.delete("/scoring_criteria")
-async def remove_scoring_criteria_from_task(ids: List[int] = Query(...)):
-    await remove_scoring_criteria_from_task_in_db(ids)
-    return {"success": True}
-
-
-@router.post("/scoring_criteria")
-async def add_scoring_criteria_to_tasks(request: AddScoringCriteriaToTasksRequest):
-    await add_scoring_criteria_to_tasks_in_db(
-        request.task_ids, request.scoring_criteria
-    )
-    return {"success": True}
-
-
 @router.delete("/{task_id}")
 async def delete_task(task_id: int):
     await delete_task_in_db(task_id)
@@ -186,29 +160,6 @@ async def get_task(task_id: int) -> LearningMaterialTask | QuizTask:
     if not task:
         raise HTTPException(status_code=404, detail="Task not found")
     return task
-
-
-@router.get("/{task_id}/scoring_criteria")
-async def get_task_scoring_criteria(task_id: int):
-    return await get_scoring_criteria_for_task_from_db(task_id)
-
-
-@router.post("/{task_id}/tags")
-async def add_tags_to_task(task_id: int, request: TaskTagsRequest):
-    await add_tags_to_task_in_db(task_id, request.tag_ids)
-    return {"success": True}
-
-
-@router.delete("/{task_id}/tags")
-async def remove_tags_from_task(task_id: int, request: TaskTagsRequest):
-    await remove_tags_from_task_in_db(task_id, request.tag_ids)
-    return {"success": True}
-
-
-@router.put("/{task_id}/tests")
-async def update_task_tests(task_id: int, request: UpdateTaskTestsRequest):
-    await update_tests_for_task_in_db(task_id, request.tests)
-    return {"success": True}
 
 
 @router.post("/{task_id}/complete")
